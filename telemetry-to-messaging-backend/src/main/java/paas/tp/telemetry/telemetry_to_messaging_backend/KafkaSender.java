390d7d541e8583feea2b84ce2a8d64a33902897a
Williams Anthony <willay.anthony@gmail.com>
2025-11-08 20:36:16 +0100

    Adding timestamp and service name when sending a new message in kafka topics

--- telemetry-to-messaging-backend/src/main/java/paas/tp/telemetry/telemetry_to_messaging_backend/KafkaSender.java
+++ telemetry-to-messaging-backend/src/main/java/paas/tp/telemetry/telemetry_to_messaging_backend/KafkaSender.java
@@ -1,5 +1,8 @@
 package paas.tp.telemetry.telemetry_to_messaging_backend;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,13 +15,25 @@ import org.springframework.stereotype.Service;
 public class KafkaSender {
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, ObjectNode> kafkaTemplate;
 
     private static final Logger logger = LogManager.getLogger(KafkaSender.class);
 
-    public void send(String message) {
-        logger.info("Sending message: " + message);
-        kafkaTemplate.send("attemps-logs", message);
-        kafkaTemplate.send("logs", message);
+    public void send(String message)  {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            ObjectNode jsonNode = (ObjectNode) mapper.readTree(message);
+            jsonNode.put("timestamp", System.currentTimeMillis());
+            jsonNode.put("service", "telemetry-to-messaging-backend");
+            //String messageToSend = mapper.writeValueAsString(jsonNode);
+
+            logger.info("Sending message: " + jsonNode);
+            kafkaTemplate.send("attemps-logs", jsonNode);
+            kafkaTemplate.send("logs", jsonNode);
+
+        } catch (Exception e) {
+            logger.error("Error during JSON parsing or sending message: " + message, e);
+        }
+
     }
 }
