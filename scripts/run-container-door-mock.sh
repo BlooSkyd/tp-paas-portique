004b51cec6ccca8f8611a482b87fa2378a1eb85c
Williams Anthony <willay.anthony@gmail.com>
2025-12-11 21:31:25 +0100

    Adding py script to add a new photo for one provided num

--- scripts/run-container-door-mock.sh
+++ scripts/run-container-door-mock.sh
@@ -2,4 +2,4 @@ docker run -d \
   -e MQTT_BROKER_HOST=172.31.249.144 \
   -e MQTT_BROKER_PORT=8883 \
   -e MQTT_BADGE_TOPIC="door/open" \
-  smailliwdocker/entrance-door-mock:latest
+  smailliwdocker/entrance-door-lock:latest
