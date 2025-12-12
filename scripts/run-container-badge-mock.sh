5ef6bee2b079a25d3cb319eb9b542c0bcbd07039
Williams Anthony <willay.anthony@gmail.com>
2025-12-12 12:33:38 +0100

    Refacto Mocks scripts

--- scripts/run-container-badge-mock.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-  --name badge-sensor-mock \
-  -e MQTT_BROKER_HOST=172.31.249.144 \
-  -e MQTT_BROKER_PORT=8883 \
-  -e MQTT_BADGE_TOPIC="sensor/data" \
-  smailliwdocker/badge-sensor-mock:latest
--- scripts/run-container-door-mock.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-  --name entrance-door-lock
-  -e MQTT_BROKER_HOST=172.31.249.144 \
-  -e MQTT_BROKER_PORT=8883 \
-  -e MQTT_BADGE_TOPIC="door/open" \
-  smailliwdocker/entrance-door-lock:latest
